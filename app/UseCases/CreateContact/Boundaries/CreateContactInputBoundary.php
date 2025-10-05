73bd2f553e2bf2da45ff445fd2ac5744f5091a99
William Trindade <williamtrindade777@gmail.com>
2025-10-05 18:09:46 -0300

    05-10-2025

--- app/UseCases/CreateContact/Boundaries/CreateContactInputBoundary.php
+++ app/UseCases/CreateContact/Boundaries/CreateContactInputBoundary.php
@@ -2,8 +2,6 @@
 
 namespace App\UseCases\CreateContact\Boundaries;
 
-// A "porta de entrada" para o caso de uso.
-// O Controller dependerá desta interface para invocar a lógica de negócio.
 use App\UseCases\CreateContact\DTOs\CreateContactRequestModel;
 use App\UseCases\CreateContact\DTOs\CreateContactResponseModel;
 
